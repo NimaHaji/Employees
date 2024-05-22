d014bf5ef9e30b3ca044f51508770d8aefb88f4d
nima haji <nimahaji1395@gmail.com>
2024-05-22 12:02:50 +0430

    classes Are OK !!

---
+++ Employees/MyDate.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employees
+{
+    public class MyDate
+    {
+        private int day, month, year;
+        public int Day
+        {
+            get { return day; }
+            set { day = (value >= 1 && value <= 31) ? value : 1; }
+        }
+        public int Month
+        {
+            get { return month; }
+            set { month = (value >= 1 && value <= 12) ? value : 1; }
+        }
+        public int Year
+        {
+            get { return year; }
+            set { year = value; }
+
+        }
+        public MyDate()
+        {
+            day = 24; month = 2; year = 1403;
+        }
+        /*public MyDate(int day, int m, int y){
+         this.day=day;  month= m;   year= y;
+        }*/
+        public MyDate(int day, int m, int y)
+        {
+            Day = day; Month = m; Year = y;
+        }
+        /*public SetDate(int day, int nonth, int year){
+       }*/
+        public string ToString()
+        {
+            return String.Format("{0}/{1:D2}/{2:D2}", year, month, day);
+        }
+        public void print()
+        {
+            Console.WriteLine("{0}/{1}/{2}", year, month, day);
+        }
+    }
+}
--- Employees/Program.cs
+++ Employees/Program.cs
@@ -1,15 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Employees;
+using System;
 
-namespace Employees
+namespace Employee
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
+            Console.WriteLine("Hello World!");
+            /*MyDate d1=new MyDate();
+            Console.WriteLine("Date:{0}/{1}/{2}",d1.Year,d1.Month,d1.Day);
+            Console.WriteLine("Your Date:"+d1.ToString());
+            d1.Year=1400;
+            d1.Day=40;
+            d1.print();
+            MyDate d2=new MyDate(13,10,1401);
+            d2.print();
+            d2.Day=Convert.ToInt16(Console.ReadLine());
+            */
+            Person p1;
+            p1 = new Person();
+            p1.Print();
+            Person p2 = new Person("Sahar", "Golbabaeii", 12, 15, 1382);
+            p2.Print();
+            Console.WriteLine(p2.fullName);
+            Console.Write("Press any key to continue . . . ");
+            Console.ReadKey(true);
         }
     }
 }
