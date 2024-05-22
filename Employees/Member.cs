b35253800a3ed562a15a27c6f93bcf9300493260
nima haji <nimahaji1395@gmail.com>
2024-05-22 22:00:43 +0430

    Change Employee class name to Member

---
+++ Employees/Member.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employees
+{
+    public class Member:Person
+    {
+        
+        private int IdNumber;
+        public int Id
+        {
+            get { return IdNumber; }
+            set { IdNumber = value; }
+        }
+        MyDate hireDate;
+        public Member(int iD, int day, int m, int y) : base()
+        {
+            IdNumber = iD;
+            hireDate = new MyDate(day, m, y);
+        }
+    }
+}
